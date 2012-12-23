2977a604a40ee005d704dc78ca954f7e92f7218c
Dan Bjorge <dan@dbjorge.net>
2012-12-22 20:12:10 -0500

    Added a MobileServiceDataSource hooked to Azure

---
+++ DragonAidLib/Data/Sources/IDataSource.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+
+// TODO: this shouldn't be dependent on MobileServices
+// Probably IDataSource should require templatized versions of IMobileServiceTable's <blah>Async methods
+namespace DragonAidLib.Data.Sources
+{
+    interface IDataSource
+    {
+        IMobileServiceTable<T> GetTable<T>();
+    }
+}
---
+++ DragonAidLib/Data/Sources/MobileServiceDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace DragonAidLib.Data.Sources
+{
+    class MobileServiceDataSource : IDataSource
+    {
+        private static readonly MobileServiceClient MobileService = new MobileServiceClient(
+            "https://dragonaid-msvc.azure-mobile.net/",
+            "zqluHzWMYRCtytKXNZjkJhGxTXiQKX67"
+        );
+
+        public IMobileServiceTable<T> GetTable<T>()
+        {
+            return MobileService.GetTable<T>();
+        }
+    }
+}
