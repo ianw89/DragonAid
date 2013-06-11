b55efeac6a1f10db10847fdc905d63525f0e78de
Jennifer Spinney <jen.spinney@gmail.com>
2013-06-10 20:17:20 -0700

    Lower-cased local variable name
    The upper-casing of local variables had me scratching my head when I was
    first trying to read this method, so I lower-cased them.

--- DragonAidLib/Data/Model/CharacterSkillRanks.cs
+++ DragonAidLib/Data/Model/CharacterSkillRanks.cs
@@ -9,7 +9,6 @@ namespace DragonAid.Lib.Data.Model
 {
     public class CharacterSkillRanks : IEnumerable<CharacterSkillInfo>
     {
-        
         private readonly List<CharacterSkillInfo> _list = new List<CharacterSkillInfo>();
         private readonly Character character;
 
@@ -40,14 +39,14 @@ namespace DragonAid.Lib.Data.Model
             {
                 Contract.Requires(skill != null);
 
-                var CharacterSkillInfo = this._list.SingleOrDefault(s => s.Skill == skill);
-                if (CharacterSkillInfo == null)
+                var characterSkillInfo = this._list.SingleOrDefault(s => s.Skill == skill);
+                if (characterSkillInfo == null)
                 {
                     throw new InvalidOperationException(String.Format(CultureInfo.InvariantCulture,
                                                                       "Character has no ranks in {0}!", skill.FullName));
                 }
 
-                return CharacterSkillInfo.Rank;
+                return characterSkillInfo.Rank;
             }
 
             set
@@ -55,14 +54,14 @@ namespace DragonAid.Lib.Data.Model
                 Contract.Requires(skill != null);
                 Contract.Requires(value > -0);
 
-                var CharacterSkillInfo = this._list.SingleOrDefault(w => w.Skill == skill);
-                if (CharacterSkillInfo == null)
+                var characterSkillInfo = this._list.SingleOrDefault(w => w.Skill == skill);
+                if (characterSkillInfo == null)
                 {
                     this._list.Add(new CharacterSkillInfo(skill, value, this.character));
                 }
                 else
                 {
-                    CharacterSkillInfo.Rank = value;
+                    characterSkillInfo.Rank = value;
                 }
             }
         }
